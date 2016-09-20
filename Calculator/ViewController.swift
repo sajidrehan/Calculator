51b8ed32926ae0e8750bd62ca1ca2ad25ecad4d9
Rehan Sajid <rehansajid.dev@gmail.com>
2016-09-20 10:23:53 +0500

    Buttons and Label implemented in storyboard.In VC I have implemented Target-Action for buttons.

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -10,16 +10,20 @@ import UIKit
 
 class ViewController: UIViewController {
 
-        override func viewDidLoad() {
-                super.viewDidLoad()
-                // Do any additional setup after loading the view, typically from a nib.
-        }
+       
+        
+        @IBOutlet weak var display: UILabel!
 
-        override func didReceiveMemoryWarning() {
-                super.didReceiveMemoryWarning()
-                // Dispose of any resources that can be recreated.
+        @IBAction func touchDigit(sender: UIButton) {
+                let digit = sender.currentTitle!
+                
+                let textCurrentlyInDisplay = display.text!
+                
+                display.text = textCurrentlyInDisplay + digit
+                
+                
+                
         }
-
 
 }
 
